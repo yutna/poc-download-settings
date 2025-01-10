c99998972fee72047eb753c81be2e7a0fa8e7cd6
Yutthana Siphuengchai <yutthana.s@outlook.com>
2025-01-10 17:28:59 +0700

    Back up

---
+++ src/api/downloaders/getDownloaderDriverConfigDropdown.ts
@@ -0,0 +1,29 @@
+import { AUTH_TOKEN, BASE_API_URL } from "@/config/env";
+import type { DownloaderDriverConfigDropdown } from "./types";
+
+export default async function getDownloaderDriverConfigDropdown() {
+  const url = `${BASE_API_URL}/v3/downloader_driver_config/dropdown`;
+  const options: RequestInit = {
+    method: "GET",
+    headers: {
+      authorization: `Bearer ${AUTH_TOKEN}`,
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+
+    if (!response.ok) {
+      throw new Error(
+        `Failed to fetch downloader driver config dropdown: ${response.statusText}`
+      );
+    }
+
+    const data = (await response.json()) as DownloaderDriverConfigDropdown;
+
+    return data;
+  } catch (error) {
+    console.error("Error fetching downloader driver config dropdown:", error);
+    throw error;
+  }
+}
--- src/api/downloaders/types.ts
+++ src/api/downloaders/types.ts
@@ -1,3 +1,18 @@
+export interface DownloaderDriverConfigDropdown {
+  id: number;
+  label: "string";
+  context: {
+    fields: {
+      username: string;
+      password: string;
+    };
+  };
+}
+
+// TODO:
+// Yut ต้องส่ง driver id มาใน dropdown config API
+// Robert แก้ field เป็น username password ไปก่อน ตาม UI
+
 export interface DownloaderFileDropdown {
   id: number;
   label: string;
