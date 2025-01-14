d712df1881c0ad2eb7f3976304125ed3978a7b3d
ryosantouchh <kittitat.suntimak@gmail.com>
2025-01-14 10:01:34 +0700

    update export

--- src/api/downloaders/index.ts
+++ src/api/downloaders/index.ts
@@ -1,3 +1,3 @@
 export { default as getDownloaderDropdown } from "./get-downloader-dropdown";
-export { default as getDownloaderFileDropdown } from "./get-downloader-file-dropdown";
-export { default as postDownloaderDataFolder } from "./post-downloader-data-folder";
+export { default as getDownloaderFileDropdown } from "./getDownloaderFileDropdown";
+export { default as postDownloaderDataFolder } from "./postDownloaderDataFolder";
